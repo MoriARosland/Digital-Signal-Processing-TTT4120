7cd0c53f024348d7c86b58644332e4415a1084b1
moriar <moriar@stud.ntnu.no>
2023-08-29 18:53:40 +0200

    Initial commit

---
+++ Problem Set 1/Task 2a.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# import sounddevice as sd
+
+# Parameters
+A = 1.0              # Amplitude
+f_1 = 5.0            # Normalized frequency
+F_s = 6000.0         # Sampling rate (samples per second)
+duration = 4.0       # Duration of the sequence in seconds
+samples = F_s*duration
+# Generate time values using np.linspace()
+n = np.linspace(0, duration, int(samples), endpoint=False)
+
+# Generate the discrete harmonic sequence
+x_n = A * np.cos(2 * np.pi * f_1 * n)
+
+# Plot the sequence
+plt.figure(figsize=(10, 4))
+plt.plot(n, x_n)
+plt.title("Discrete Harmonic Sequence")
+plt.xlabel("Time (seconds)")
+plt.ylabel("Amplitude")
+# plt.grid(True)
+plt.show()
+
+

7ed0e9185169b6e38eeabacaaa8bb6bff8990189
Mori Adrian Rosland <mori@live.no>
2023-10-20 13:33:47 +0200

    Added Task 3b

---
+++ Problem Set 6/ProblemSet_6_Task3b.m
@@ -0,0 +1,18 @@
+close all;
+
+f1 = 7 / 40;
+f2 = 9 / 40;
+
+N = 10;
+
+n = 0 : 1000;
+x = sin(2*pi*f1.*n)+sin(2*pi*f2.*n);
+
+X = fft(x,N);
+f = (0:N-1)./(N);
+F = f.*N;
+
+plot(f,abs(X));
+xlim([0,0.5]);
+xlabel('f');
+ylabel('|X(f)|');
